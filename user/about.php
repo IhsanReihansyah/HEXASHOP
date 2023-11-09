69ba2ee609366c4f6a25db4ca93b1a4929da863c
rehansyah <ihsanreihansyah24@gmail.com>
2023-11-09 22:06:43 +0700

    new

--- user/about.php
+++ user/about.php
@@ -14,21 +14,11 @@
 
     <!-- Additional CSS Files -->
     <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
-
     <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.css">
-
     <link rel="stylesheet" href="../assets/css/templatemo-hexashop.css">
-
     <link rel="stylesheet" href="../assets/css/owl-carousel.css">
-
     <link rel="stylesheet" href="../assets/css/lightbox.css">
-<!--
 
-TemplateMo 571 Hexashop
-
-https://templatemo.com/tm-571-hexashop
-
--->
     </head>
     
     <body>
--- user/contact.php
+++ user/contact.php
@@ -14,13 +14,9 @@
 
     <!-- Additional CSS Files -->
     <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
-
     <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.css">
-
     <link rel="stylesheet" href="../assets/css/templatemo-hexashop.css">
-
     <link rel="stylesheet" href="../assets/css/owl-carousel.css">
-
     <link rel="stylesheet" href="../assets/css/lightbox.css">
 
     </head>
